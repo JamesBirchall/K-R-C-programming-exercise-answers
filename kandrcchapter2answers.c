43e08190db09cfd32d34e740a5716d0c973d6515
James Birchall <james@localhost.(none)>
2012-11-13 14:31:52 +0000

    update

--- kandrcchapter2answers.c
+++ kandrcchapter2answers.c
@@ -28,12 +28,12 @@ void ex2_1(){
 	signed long long2 = 1;
 	signed long long2previous = 0;
 
-	printf("Unsigned char\tMax:%u\n",UCHAR_MAX);
-	printf("Signed char\tMin:%d\tMax:%d\n", CHAR_MIN, CHAR_MAX);
+	printf("Unsigned char\tMax:%hhu\n",UCHAR_MAX);
+	printf("Signed char\tMin:%hhd\tMax:%hhd\n", CHAR_MIN, CHAR_MAX);
 	printf("Unsigned int\tMax:%u\n", UINT_MAX);
 	printf("Signed int\tMin:%u\tMax:%d\n", INT_MIN, INT_MAX);
-	printf("Unsigned short\tMax:%u\n", USHRT_MAX);
-	printf("Signed short\tMin:%d\tMax:%d\n", SHRT_MIN, SHRT_MAX);
+	printf("Unsigned short\tMax:%hu\n", USHRT_MAX);
+	printf("Signed short\tMin:%hd\tMax:%hd\n", SHRT_MIN, SHRT_MAX);
 	printf("Unsigned long\tMax:%lu\n", ULONG_MAX);
 	printf("Signed long\tMin:%ld\tMax:%ld\n", LONG_MIN, LONG_MAX);
 	printf("Unsigned long long\tMax:%llu\n", ULLONG_MAX);
@@ -62,4 +62,55 @@ void ex2_1(){
 		char2--;
 	}
 	printf("Computed signed char MIN:%d\n", char2previous);
+
+	short1 = 1;
+	short1previous = 0;
+
+	while(short1 != 0){
+		short1previous = short1;
+		short1++;
+	}
+	printf("Computed unsigned short MAX:%hu\n", short1previous);
+
+	short2 = -1;
+	short2previous = 0;
+
+	while(short2 !=0){
+		if(short2 < short2previous){
+			short2previous = short2;
+		}
+		short2--;
+	}
+	printf("Computed signed short MIN:%hd\n", short2previous);
+
+	short2 = 1;
+	short2previous = 0;
+
+	while(short2 != 0){
+		if(short2 > short2previous){
+			short2previous = short2;
+		}
+		short2++;
+	}
+	printf("Computed signed short MAX:%hd\n", short2previous);
+
+	integer1 = 1;
+	integer1previous = 0;
+
+	while(integer1 != 0){
+		integer1previous = integer1;
+		integer1++;
+	}
+	printf("Computer unsigned int MAX:%u\n", integer1previous);
+	
+	integer2 = -1;
+	integer2previous = 0;
+
+	while(integer2 != 0){
+		if(integer2 < integer2previous){
+			integer2previous = integer2;
+		}
+		integer2++;
+	}
+	printf("Computed signed int MIN:%d\n", integer2previous);
 }
