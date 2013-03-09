228f7520dd80afb0e44e2557564b36e0c0e06917
James Birchall <jamesbirchall@gmail.com>
2013-03-09 11:29:09 +0000

    polish calc basic complete

--- kandrcchapter4answers.c
+++ kandrcchapter4answers.c
@@ -15,8 +15,26 @@ int polishcalc(){
       case '+':
         push(pop() + pop());
         break;
+      case '*':
+        push(pop() * pop());
+        break;
+      case '-':
+        op2 = pop();
+        push(pop() - op2);
+        break;
+      case '/':
+        op2= pop();
+        if(op2 != 0.0)
+          push(pop() / op2);
+        else
+          printf("error: divide by zero\n");
+        break;
       case '\n':
         printf("\n\t%.8g\n", pop());
+        break;
+      case 'q':
+        printf("\n Quitting...\n");
+        exit(0);
         break;
       default:
         printf("error: unknown command %s\n", myString);
